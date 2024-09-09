c1f8e1a3d810ab95d2ff03d3daedb74750114ffa
Mattha1401 <122351408+Mattha1401@users.noreply.github.com>
2024-09-09 15:44:58 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:modern_navbar/screens/navbar_screen.dart';
+import 'package:modern_navbar/screens/profile_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      title:'Modern NavigatorBar Demo',
+      home: NavbarScreen(),
+    );
+  }
+}
